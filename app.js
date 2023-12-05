7e640ec7956b60fa7d5ca9edc8f449077d021e5a
Tega-Efe <146211433+Tega-Efe@users.noreply.github.com>
2023-12-05 20:15:54 +0100

    first

---
+++ app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', function(){
+    // alert('working');
+    const nav = document.querySelector('nav');
+
+    if(nav.style.display == 'block'){
+        nav.style.display = 'none';
+        button.src ='./images/icon-hamburger.svg';
+    }else{
+        nav.style.display = 'block';
+        button.src ='./images/icon-close-menu.svg';
+    }
+});
+
+
+const presentDate = new Date();
+// console.log(presentDate);
+const presentYear = presentDate.getFullYear();
